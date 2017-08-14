625433af76ddb94f18bbff120c145ab95bac31a0
Rafael Hochreuter <rafoe@hotmail.de>
2017-08-14 12:54:33 +0200

    First commit with newly created database_setup.py and nutritionplan.db files

---
+++ database_setup.py
@@ -0,0 +1,35 @@
+"""Create Database for a weekly nutrition plan"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Meal(Base):
+    """Create Meal Table."""
+
+    __tablename__ = 'meal'
+    # Create table columns.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Ingredients(Base):
+    """Create Ingredients Table."""
+
+    __tablename__ = 'ingredients'
+    # Create table columns.
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer)
+    type = Column(String(80))
+    portions = Column(Integer)
+    meal_id = Column(Integer, ForeignKey('meal.id'))
+    meal = relationship(Meal)
+
+
+# Create database file.
+engine = create_engine('sqlite:///nutritionplan.db')
+Base.metadata.create_all(engine)
