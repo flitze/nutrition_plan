802b4ccc3e06c51f2e00f99005ef2fc649d9fc83
Rafael Hochreuter <rafoe@hotmail.de>
2017-08-15 13:55:21 +0200

    Added two meals with two ingredients, respectively for testing purposes.

--- populate_nutrition_database.py
+++ populate_nutrition_database.py
@@ -26,4 +26,36 @@ ingredient1 = Ingredients(name="Quark", amount=120, amount_type="weight",
                           meal=meal1)
 
 session.add(ingredient1)
-session.commit
+session.commit()
+
+ingredient2 = Ingredients(name="Eier", amount=3, amount_type="pieces",
+                          meal=meal1)
+
+session.add(ingredient2)
+session.commit()
+
+ingredient3 = Ingredients(name="Kaese", amount=240, amount_type="weight",
+                          meal=meal1)
+
+session.add(ingredient3)
+session.commit()
+
+#
+meal2 = Meal(name="Gnocchi-Salat mit Pinienkernen", receipt="Die Gnocchi nach"
+             "Anleitung zubereiten und abkuehlen lassen.", type="vegetarisch",
+             portions=4)
+
+session.add(meal2)
+session.commit()
+
+ingredient1 = Ingredients(name="Gnocchi", amount=800, amount_type="weight",
+                          meal=meal2)
+
+session.add(ingredient1)
+session.commit()
+
+ingredient2 = Ingredients(name="Pinienkerne", amount=50, amount_type="weight",
+                          meal=meal2)
+
+session.add(ingredient2)
+session.commit()
