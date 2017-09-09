f659189005f8aec1df96d668d68bbf5702279ead
Rafael Hochreuter <rafoe@hotmail.de>
2017-09-09 13:36:07 +0200

    Populate DB with meals that have an attribute veggie instead of type.

--- populate_nutrition_database.py
+++ populate_nutrition_database.py
@@ -1,9 +1,14 @@
-
+# -*- coding: utf-8 -*-
 """Initial meal database population"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, Meal, Ingredients
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 # The Database Engine
 engine = create_engine('sqlite:///nutritionplan.db')
@@ -16,8 +21,18 @@ session = DBSession()
 
 
 # Meal Low Carb Pizzarole
-meal1 = Meal(name="Low Carb Pizzarolle", receipt="Fuer den Boden Quark",
-             type="vegetarisch", portions=3)
+meal1 = Meal(name="Low Carb Pizzarolle", receipt="Den Backofen auf 170 C"
+             "vorheizen. Fuer den Boden Quark, Eier und 120 g Kaese in einer"
+             "Schuessel miteinander verruehren und wuerzen. Die Masse auf das mit"
+             "Backpapier ausgelegte Backblech kippen und glatt streichen."
+             "15 Minuten im Ofen backen. Das Backblech herausnehmen und den"
+             "Boden beliebig belegen mit z. B. Tomatensauce, Salami, Schinken,"
+             "Zucchini, Champignons oder Mais. Mit dem restlichen Kaese"
+             "bestreuen und erneut in den Ofen schieben, bis der Kaese eine"
+             "schoene Farbe hat. Abkuehlen lassen, mit Rucola bestreuen und"
+             "vorsichtig einrollen. Eingerollt in Alufolie mehrere Tage im"
+             "Kuehlschrank haltbar.",
+             veggie=True, portions=3)
 
 session.add(meal1)
 session.commit()
@@ -37,12 +52,14 @@ session.commit()
 ingredient3 = Ingredients(name="Kaese", amount=240, amount_type="weight",
                           meal=meal1)
 
+ingredient4 = Ingredients(name="Gemuese", meal=meal1)
+
 session.add(ingredient3)
 session.commit()
 
 #
 meal2 = Meal(name="Gnocchi-Salat mit Pinienkernen", receipt="Die Gnocchi nach"
-             "Anleitung zubereiten und abkuehlen lassen.", type="vegetarisch",
+             "Anleitung zubereiten und abkuehlen lassen.", veggie=True,
              portions=4)
 
 session.add(meal2)
